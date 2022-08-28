d3874b4d8864dc4a9f962e47bdffa9201184ec6c
Mohammed Mashood PV <mashoodpv.vkm@gmail.com>
2022-08-28 18:49:39 +0530

    Added splash screen by SplashScreenAPI

--- app/src/main/java/com/mashood/thesaurus/MainActivity.kt
+++ app/src/main/java/com/mashood/thesaurus/MainActivity.kt
@@ -2,10 +2,19 @@ package com.mashood.thesaurus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.mashood.thesaurus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Add splash screen by SplashScreenAPI
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
     }
 }
