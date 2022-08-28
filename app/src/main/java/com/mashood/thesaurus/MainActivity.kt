511205a77789a5b8f9f9ef2cd4f29b282ee2b9ed
Mohammed Mashood PV <mashoodpv.vkm@gmail.com>
2022-08-28 21:02:37 +0530

    Added dimens.xml file. Starting UI designing of HomeFragment.

--- app/src/main/java/com/mashood/thesaurus/MainActivity.kt
+++ app/src/main/java/com/mashood/thesaurus/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.mashood.thesaurus
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.DecelerateInterpolator
+import androidx.core.animation.doOnEnd
+import androidx.core.splashscreen.SplashScreen
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.mashood.thesaurus.databinding.ActivityMainBinding
 
@@ -11,10 +16,28 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         // Add splash screen by SplashScreenAPI
-        installSplashScreen()
+        val splashScreen = installSplashScreen()
 
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        setupSplashScreen(splashScreen)
+    }
+
+    private fun setupSplashScreen(splashScreen: SplashScreen) {
+        splashScreen.setOnExitAnimationListener { splashScreenView ->
+            val slideBack = ObjectAnimator.ofFloat(
+                splashScreenView.view,
+                View.TRANSLATION_Y,
+                0f,
+                splashScreenView.view.height.toFloat()
+            ).apply {
+                interpolator = DecelerateInterpolator()
+                duration = 1000L
+                doOnEnd { splashScreenView.remove() }
+            }
+            slideBack.start()
+        }
     }
 }
--- app/src/main/java/com/mashood/thesaurus/home/ui/HomeFragment.kt
+++ app/src/main/java/com/mashood/thesaurus/home/ui/HomeFragment.kt
@@ -2,9 +2,7 @@ package com.mashood.thesaurus.home.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import com.mashood.thesaurus.R
 import com.mashood.thesaurus.databinding.FragmentHomeBinding
 
