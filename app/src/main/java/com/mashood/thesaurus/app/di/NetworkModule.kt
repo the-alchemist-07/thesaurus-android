787518a64897978620d4be5257c3c637cabd7271
Mohammed Mashood PV <mashoodpv.vkm@gmail.com>
2022-08-29 00:15:41 +0530

    Created Hilt's provider functions for Retrofit.

---
+++ app/src/main/java/com/mashood/thesaurus/app/di/NetworkModule.kt
@@ -0,0 +1,54 @@
+package com.mashood.thesaurus.app.di
+
+import com.mashood.thesaurus.BuildConfig
+import com.mashood.thesaurus.app.common.Constants.BASE_URL
+import com.skydoves.sandwich.adapters.ApiResponseCallAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        val okHttpClient = OkHttpClient.Builder()
+        okHttpClient.connectTimeout(30, TimeUnit.SECONDS)
+        okHttpClient.writeTimeout(30, TimeUnit.SECONDS)
+        okHttpClient.readTimeout(30, TimeUnit.SECONDS)
+        if (BuildConfig.DEBUG) {
+            val httpInterceptor =
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            okHttpClient.addInterceptor(httpInterceptor)
+        }
+        return okHttpClient.build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideConverterFactory(): Converter.Factory {
+        return MoshiConverterFactory.create()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient, converterFactory: Converter.Factory): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(converterFactory)
+            .addCallAdapterFactory(ApiResponseCallAdapterFactory.create())
+            .build()
+    }
+
+}
