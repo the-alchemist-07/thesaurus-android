316aeeaec8d4bbc82e8807510a2fd1fafba3f94b
Mohammed Mashood PV <mashoodpv.vkm@gmail.com>
2023-08-28 16:46:04 +0530

    Added method for getting the observable list of all the histories.

--- app/src/main/java/com/mashood/thesaurus/history/domain/repository/HistoryRepository.kt
+++ app/src/main/java/com/mashood/thesaurus/history/domain/repository/HistoryRepository.kt
@@ -1,4 +1,11 @@
 package com.mashood.thesaurus.history.domain.repository
 
+import com.mashood.thesaurus.app.common.Resource
+import com.mashood.thesaurus.history.data.source.HistoryEntity
+import kotlinx.coroutines.flow.Flow
+
 interface HistoryRepository {
+
+    fun getHistoriesList(): Flow<Resource<List<HistoryEntity>>>
+
 }
