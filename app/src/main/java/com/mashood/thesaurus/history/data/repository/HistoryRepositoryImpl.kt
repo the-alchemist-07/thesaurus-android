63dcf4a2d54b1b546d470133bbfd233cbff9f6eb
Mohammed Mashood PV <mashoodpv.vkm@gmail.com>
2023-08-28 16:55:05 +0530

    Added the logic to get list of histories from roomDB in the repository.

--- app/src/main/java/com/mashood/thesaurus/history/data/repository/HistoryRepositoryImpl.kt
+++ app/src/main/java/com/mashood/thesaurus/history/data/repository/HistoryRepositoryImpl.kt
@@ -1,14 +1,27 @@
 package com.mashood.thesaurus.history.data.repository
 
 import com.mashood.thesaurus.app.common.Resource
+import com.mashood.thesaurus.history.data.source.HistoryDao
 import com.mashood.thesaurus.history.data.source.HistoryEntity
 import com.mashood.thesaurus.history.domain.repository.HistoryRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
 
-class HistoryRepositoryImpl: HistoryRepository {
+class HistoryRepositoryImpl @Inject constructor(
+    private val historyDao: HistoryDao
+): HistoryRepository {
 
-    override fun getHistoriesList(): Flow<Resource<List<HistoryEntity>>> {
-        TODO("Not yet implemented")
+    override fun getHistoriesList(): Flow<Resource<List<HistoryEntity>>> = flow {
+        val histories = historyDao.getHistories().first()
+        if (histories.isNotEmpty())
+            emit(Resource.Success(histories))
+        else
+            emit(Resource.Error(EMPTY_HISTORY))
     }
 
+    companion object {
+        const val EMPTY_HISTORY = "No history found!"
+    }
 }
