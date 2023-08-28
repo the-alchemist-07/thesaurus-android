ddfa9dca37b04e05fad9aec8968cbdd68d85cff7
Mohammed Mashood PV <mashoodpv.vkm@gmail.com>
2023-08-28 17:05:14 +0530

    Injected HistoryRepository's instance in the constructor of HistoryViewModel class.

--- app/src/main/java/com/mashood/thesaurus/history/ui/HistoryViewModel.kt
+++ app/src/main/java/com/mashood/thesaurus/history/ui/HistoryViewModel.kt
@@ -1,6 +1,10 @@
 package com.mashood.thesaurus.history.ui
 
 import androidx.lifecycle.ViewModel
+import com.mashood.thesaurus.history.domain.repository.HistoryRepository
+import javax.inject.Inject
 
-class HistoryViewModel: ViewModel() {
+class HistoryViewModel @Inject constructor(
+    private val historyRepository: HistoryRepository
+): ViewModel() {
 }
