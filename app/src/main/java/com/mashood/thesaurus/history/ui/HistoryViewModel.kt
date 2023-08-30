89bc52a8c3a2259e822d7b2d7b6e29602e01e984
Mohammed Mashood PV <mashoodpv.vkm@gmail.com>
2023-08-30 18:50:27 +0530

    Created flow in the HistoryViewModel class using the corresponding UiState

--- app/src/main/java/com/mashood/thesaurus/history/ui/HistoryViewModel.kt
+++ app/src/main/java/com/mashood/thesaurus/history/ui/HistoryViewModel.kt
@@ -2,9 +2,15 @@ package com.mashood.thesaurus.history.ui
 
 import androidx.lifecycle.ViewModel
 import com.mashood.thesaurus.history.domain.repository.HistoryRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 class HistoryViewModel @Inject constructor(
     private val historyRepository: HistoryRepository
 ): ViewModel() {
+
+    private val _historyState = MutableStateFlow<HistoryState>(HistoryState.Idle)
+    val historyState = _historyState.asStateFlow()
+
 }
