02cb7b57cb6e7fa58e479edda10ccd2404fbd3e0
Mohammed Mashood PV <mashoodpv.vkm@gmail.com>
2023-08-30 18:47:59 +0530

    Added states for all scenarios in HistoryState.

--- app/src/main/java/com/mashood/thesaurus/history/ui/HistoryState.kt
+++ app/src/main/java/com/mashood/thesaurus/history/ui/HistoryState.kt
@@ -1,4 +1,11 @@
 package com.mashood.thesaurus.history.ui
 
+import com.mashood.thesaurus.history.data.source.HistoryEntity
+
 sealed class HistoryState {
+
+    data class SuccessHistoryList(val historyList: List<HistoryEntity>) : HistoryState()
+    data class Error(val message: String) : HistoryState()
+    object Idle : HistoryState()
+
 }
