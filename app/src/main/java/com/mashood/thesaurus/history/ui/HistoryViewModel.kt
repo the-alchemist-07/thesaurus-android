7a087223d59f6d04ad945e1452e5cb375a2bbbd7
Mohammed Mashood PV <mashoodpv.vkm@gmail.com>
2023-09-03 22:32:45 +0530

    Added logic to fetch histories list in the viewModel.

--- app/src/main/java/com/mashood/thesaurus/history/ui/HistoryViewModel.kt
+++ app/src/main/java/com/mashood/thesaurus/history/ui/HistoryViewModel.kt
@@ -1,16 +1,37 @@
 package com.mashood.thesaurus.history.ui
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.mashood.thesaurus.app.common.Resource
 import com.mashood.thesaurus.history.domain.repository.HistoryRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class HistoryViewModel @Inject constructor(
     private val historyRepository: HistoryRepository
-): ViewModel() {
+) : ViewModel() {
 
     private val _historyState = MutableStateFlow<HistoryState>(HistoryState.Idle)
     val historyState = _historyState.asStateFlow()
+
+    init {
+        getHistoriesList()
+    }
+
+    private fun getHistoriesList() = viewModelScope.launch {
+        historyRepository.getHistoriesList().collect {
+            when (it) {
+                is Resource.Success -> _historyState.emit(
+                    HistoryState.SuccessHistoryList(it.value)
+                )
+                is Resource.Error -> _historyState.emit(
+                    HistoryState.Error(it.error)
+                )
+                else -> Unit
+            }
+        }
+    }
 
 }
