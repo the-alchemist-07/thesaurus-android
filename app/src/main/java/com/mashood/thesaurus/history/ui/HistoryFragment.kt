60fa3c4525db416027ef2453a9e3f00a1ba735c5
Mohammed Mashood PV <mashoodpv.vkm@gmail.com>
2023-09-20 17:01:41 +0530

    Implemented search functionality in the history listing page.

--- app/src/main/java/com/mashood/thesaurus/history/ui/HistoryFragment.kt
+++ app/src/main/java/com/mashood/thesaurus/history/ui/HistoryFragment.kt
@@ -2,6 +2,7 @@ package com.mashood.thesaurus.history.ui
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -25,6 +26,7 @@ class HistoryFragment : Fragment(R.layout.fragment_history),
     private lateinit var binding: FragmentHistoryBinding
     private val viewModel by viewModels<HistoryViewModel>()
     private val historyAdapter: HistoryAdapter by lazy { HistoryAdapter(this) }
+    private var historyList: List<History> = emptyList()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -44,6 +46,28 @@ class HistoryFragment : Fragment(R.layout.fragment_history),
             btnBack.setOnClickListener {
                 findNavController().navigateUp()
             }
+
+            etSearch.doAfterTextChanged { text ->
+                if (text.toString().isNotBlank()) {
+                    btnClear.visibility = View.VISIBLE
+                    if (historyList.isNotEmpty()) {
+                        //new array list that will hold the filtered data
+                        val filteredList = historyList.filter { history ->
+                            history.word.lowercase().startsWith(text.toString().lowercase())
+                        }
+                        historyAdapter.submitList(filteredList)
+                    }
+                } else {
+                    btnClear.visibility = View.GONE
+                    // Set the complete history list on clearing the search query
+                    if (historyList.isNotEmpty())
+                        historyAdapter.submitList(historyList)
+                }
+            }
+
+            btnClear.setOnClickListener {
+                etSearch.text?.clear()
+            }
         }
     }
 
@@ -61,13 +85,15 @@ class HistoryFragment : Fragment(R.layout.fragment_history),
         }
     }
 
-    private fun showHistoriesList(historyList: List<History>) {
+    private fun showHistoriesList(historiesList: List<History>) {
+        historyList = historiesList
         historyAdapter.submitList(historyList)
     }
 
     private fun handleError(message: String) {
         if (EMPTY_HISTORY == message) {
-            historyAdapter.submitList(emptyList())
+            historyList = emptyList()
+            historyAdapter.submitList(historyList)
             binding.lytError.visibility = View.VISIBLE
         } else
             Snackbar.make(binding.root, message, Snackbar.LENGTH_LONG).show()
