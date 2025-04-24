e3aacf43ab893c5a03a80f40e7908c4c6faec827
Mohammed Mashood PV <mashoodpv.vkm@gmail.com>
2025-04-24 17:41:16 +0530

    Created hilt module for datastore.

---
+++ app/src/main/java/com/mashood/thesaurus/app/di/DataStoreModule.kt
@@ -0,0 +1,33 @@
+package com.mashood.thesaurus.app.di
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.preferencesDataStoreFile
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataStoreModule {
+
+    private const val USER_BEHAVIOUR_PREFERENCES = "user_behaviour_preferences"
+
+    @Provides
+    @Singleton
+    fun providePreferencesDataStore(@ApplicationContext context: Context): DataStore<Preferences> {
+        return PreferenceDataStoreFactory.create(
+            corruptionHandler = ReplaceFileCorruptionHandler(
+                produceNewData = { emptyPreferences() }
+            ),
+            produceFile = { context.preferencesDataStoreFile(USER_BEHAVIOUR_PREFERENCES) }
+        )
+    }
+}
